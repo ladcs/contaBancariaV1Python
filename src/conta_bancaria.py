2bfe04088bd9cfa2c58b669426c80a3a0ba4c3c1
ladcs <hpladcs@gmail.com>
2023-07-25 20:09:19 -0300

    fix words

--- src/conta_bancaria.py
+++ src/conta_bancaria.py
@@ -16,33 +16,34 @@ def painel():
     while option != '0':
         option = input('''
 digite os valor para as opções:
-[1] depósito;
-[2] saque;
-[3] saldo;
-[0] sair.
+[d] Depósito;
+[s] Saque;
+[e] Saldo;
+[q] Sair.
 
 Opção: ''')
-        if option == '1':
+        if option == 'd':
             value = valor_input('depósito')
             dep_list.append(value)
             extrato += value
-        elif option == '2':
+        elif option == 's':
             value = valor_input('saque')
             if number_saque >= 3 or value > max_saque:
-                print('Pode-se sacar 3 vezes por dia um valor máximo de R$500,00')
+                print
+                ('Pode-se sacar 3 vezes por dia um valor máximo de R$500,00')
             elif value > extrato:
                 print('não será possível sacar dinheiro por falta de saldo.')
             else:
                 saq_list.append(value)
                 extrato -= value
                 number_saque += 1
-        elif option == '3':
+        elif option == 'e':
             print(f'Extrato é de: R${extrato:.2f}'.replace('.', ','))
             for saq in saq_list:
                 print(f'- {saq}'.replace('.', ','))
             for dep in dep_list:
                 print(f'+ {dep}'.replace('.', ','))
-        elif option == '0':
+        elif option == 'q':
             print('\n\ntchau!\n\n')
         else:
             print('\n\ndigite uma alternativa válida!\n\n')
